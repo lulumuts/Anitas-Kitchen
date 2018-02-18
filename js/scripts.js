3ad90f2f70498320a8b202e7fa7aa608310d2267
Lulu <lulumutuli24@gmail.com>
2018-02-18 23:30:29 +0300

    initial menu settings

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
-$(document).ready(function() {
-       $("button#drinks").click(function() {
-      }
+// $(document).ready(function() {
+//        $("button#drinks").click(function() {
+//       }
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+    var menu = $("#navbar1").click(function() {
+      $(".navbar1").slidetoggle();   
+    });
+});
